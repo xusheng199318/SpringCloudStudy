acafe7bc4736c9c701fb96a0052e251f390dbfc9
Arthur <arthur@qq.com>
2019-04-24 15:45:35 +0800

    提交自定义负载均衡策略样例

--- 03-consumer-feign-8080/src/main/java/com/arthur/Application.java
+++ 03-consumer-feign-8080/src/main/java/com/arthur/Application.java
@@ -1,9 +1,15 @@
 package com.arthur;
 
+import com.arthur.service.CustomRule;
+import com.netflix.loadbalancer.IRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableFeignClients(basePackages = "com.arthur.service")
 @EnableEurekaClient
@@ -14,4 +20,10 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public IRule loadBalanceRule() {
+		List<Integer> excludePorts = new ArrayList<>();
+		excludePorts.add(8080);
+		return new CustomRule(excludePorts);
+	}
 }
