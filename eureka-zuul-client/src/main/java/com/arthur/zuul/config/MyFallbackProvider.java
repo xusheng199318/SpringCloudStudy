1ed28df901c88482c0a7e2531d3d6e5fe24fde15
Arthur <arthur@qq.com>
2019-05-08 17:18:42 +0800

    提交zuul的demo

---
+++ eureka-zuul-client/src/main/java/com/arthur/zuul/config/MyFallbackProvider.java
@@ -0,0 +1,50 @@
+package com.arthur.zuul.config;
+
+import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Created by xusheng on 2019/5/8.
+ */
+public class MyFallbackProvider implements FallbackProvider {
+    public String getRoute() {
+        return "eureka-feign-client";
+    }
+
+    public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
+        return new ClientHttpResponse() {
+            public HttpStatus getStatusCode() throws IOException {
+                return HttpStatus.OK;
+            }
+
+            public int getRawStatusCode() throws IOException {
+                return 200;
+            }
+
+            public String getStatusText() throws IOException {
+                return "OK";
+            }
+
+            public void close() {
+
+            }
+
+            public InputStream getBody() throws IOException {
+                return new ByteArrayInputStream("error!i am the fallback".getBytes());
+            }
+
+            public HttpHeaders getHeaders() {
+                HttpHeaders headers = new HttpHeaders();
+                headers.setContentType(MediaType.APPLICATION_JSON);
+                return headers;
+            }
+        };
+    }
+}
